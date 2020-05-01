716210ace2d16c7d561e3d7203161ed3336736e5
Clinton Nguyen <60639028+MintiClinti@users.noreply.github.com>
2020-05-01 10:29:57 -0700

    Update number_guessing_game.py
    Given 7 tries, guess a number between 0 and 100.

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -3,20 +3,17 @@ x = random.randint(0, 100)
 attempts = 0
 
 print("Guess a number between 0 and 100. ")
-while attempts < 10:
-    guess = input("What's your guess? ")
-
+while attempts < 7:
+    guess = int(input("What's your guess? "))
     attempts += 1
-
     if guess < x:
         print("The number is higher.")
-    if guess > x:
+    elif guess > x:
         print("The number is lower.")
-    if guess == x:
-        print("You guessed the number in " + str(attempts) + " tries.")
+    else: 
+        print("You guessed the number in " + str(attempts) + " tries!")
         break
 
-if guess != x:
-    x = str(x)
-    print("The number was actually " + x + " .")
 
+if guess != x:
+    print("The number was actually " + str(x) + ".")
