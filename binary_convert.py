fd1560faf47386dccd7bf940e23bc8935780bc54
Clinton Nguyen <60639028+MintiClinti@users.noreply.github.com>
2020-05-04 11:33:31 -0700

    Update binary_convert.py
    This program takes in a decimal number and converts it to binary.

--- binary_convert.py
+++ binary_convert.py
@@ -1,10 +1,14 @@
 num = int(input("Enter a number. "))
 binum = [num % 2]
 
-while num > 1:
-    num = num // 2
-    remainder = (num % 2)
-    binum.append(remainder)
-    
-print(binum[::-1])
+def binary_convert(num):
+    if num < 0:
+        return("We don't handle negatives. Sorry.")
+    else:
+        while num > 1:
+            num = num // 2
+            remainder = (num % 2)
+            binum.append(remainder)
+        return(binum[::-1])
 
+print(binary_convert(num))
