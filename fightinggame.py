4d7096f29141470c7f76daa39081c490680fc191
Clinton Nguyen <60639028+MintiClinti@users.noreply.github.com>
2020-05-06 12:48:07 -0700

    Update fightinggame.py

--- fightinggame.py
+++ fightinggame.py
@@ -1,3 +1,5 @@
+import random
+
 class Player:
     def __init__(player, name):
         player.name = name
@@ -5,14 +7,14 @@ class Player:
 
 class Puncher(Player):
     def __init__(punch, name, health, energy, tech):
-        super().__init__(name)
+        Player.__init__(punch, name)
         punch.health = health
         punch.energy = energy
         punch.tech = tech
     
 class Kicker(Player):
     def __init__(kick, name, health, energy, tech):
-        super().__init__(name)
+        Player.__init__(kick, name)
         kick.health = health
         kick.energy = energy
         kick.tech = tech
@@ -40,11 +42,11 @@ while kicker.health > 0 or puncher.health > 0:
         if turn % 2 == 0:
             move = input(str(puncher.name) +" , what is your move? 0 for attack, 1 for tech. ")
             if move == "0":
-                kicker.health -= (10)
+                kicker.health -= random.randint(8, 12)
                 print(str(puncher.name) + " lands a hook on " + str(kicker.name) + "!")
                 print(str(kicker.name) + " has " + str(kicker.health) + " health!")
             elif move == "1" and puncher.energy > 0:
-                kicker.health -= 15
+                kicker.health -= random.randint(13, 17)
                 puncher.energy -= 8
                 print(str(puncher.name) + " hardens his fists and bashes " + str(kicker.name) + "!")
                 print(str(puncher.name) + " has " + str(puncher.energy) + " energy!")
@@ -54,12 +56,12 @@ while kicker.health > 0 or puncher.health > 0:
         if turn % 2 == 1:
             move = input(kicker.name +" , what is your move? 0 for attack, 1 for tech. ")
             if move == "0":
-                puncher.health -= (15)
+                puncher.health -= random.randint(14, 18)
                 print(str(kicker.name) + " lands a kick on " + str(puncher.name) + "!")
                 print(str(puncher.name) + " has " + str(puncher.health) + " health!")
             elif move == "1" and kicker.energy > 0:
                 kicker.energy -= 10
-                puncher.health -= 30
+                puncher.health -= random.randint(23, 27)
                 print(str(kicker.name) + " musters his stregth and strikes " + str(puncher.name) + "!")
                 print(str(kicker.name) + " has " + str(kicker.energy) + " energy!")
                 print(str(puncher.name) + " has " + str(puncher.health) + " health!")
@@ -68,7 +70,8 @@ while kicker.health > 0 or puncher.health > 0:
         
         turn += 1
 
-        
+
+               
 
         
     
